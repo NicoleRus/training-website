cfed91f92b6538bb949c987bc825da1203e76c36
Nicole Berreth <nicoler.russo12@gmail.com>
2024-02-18 16:48:09 -0600

    feat: Add profile page

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroComponent } from '../../components/hero/hero.component';
 import { UserService } from '../../services/user/user.service';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [CommonModule, LoginFormComponent, SignupFormComponent],
+  imports: [CommonModule, ProfileComponent, LoginFormComponent, SignupFormComponent],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
 export class AccountComponent {
   constructor(private userService: UserService) {}
-  formType: 'login' | 'signup' = 'signup';
+  formType: 'login' | 'signup' = 'login';
 
   user = this.userService.user;
   loggedIn = this.userService.loggedIn;
