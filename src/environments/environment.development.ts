a24c013a9d11c2b8d7e97aeab59ac7de0847073a
Nicole Berreth <nicoler.russo12@gmail.com>
2024-03-30 12:11:35 -0500

    refactor: env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: false,
-  supabase: {
-    url: 'https://fpdrzcuymwjlxyneebhr.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwZHJ6Y3V5bXdqbHh5bmVlYmhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4NjQ0NjUsImV4cCI6MjAyNjQ0MDQ2NX0.kLBZdzz_8jaAfjhMyHHy2x-kQ8_gsTQ8dafc-Qb4LLM',
-  }
+
 };
